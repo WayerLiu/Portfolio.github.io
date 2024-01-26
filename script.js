e5da34ddeb01df878c9c50d3b9ab6793e25cb8af
WayerLiu <57646770+WayerLiu@users.noreply.github.com>
2024-01-26 20:38:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Code for image slider functionality
+    // This will be a basic setup; you may want to use a library like SwiperJS for more advanced features
+});
